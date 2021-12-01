0cde07f3191f9cfb3ee0f331b187294e9b412d87
KLAUS WONG (SYS-TSV-ISD-OOCLL/HKG) <WONGKL2@OOCL.COM>
2021-12-01 16:19:54 +0800

    [OOPExercise][KlausWong] feat: AC2 create truck object

--- src/main/java/com/afs/oopractice/Truck.java
+++ src/main/java/com/afs/oopractice/Truck.java
@@ -1,11 +1,10 @@
 package com.afs.oopractice;
 
-public class Car {
-
+public class Truck {
     private final String name;
     private final Integer speed;
 
-    public Car(String name, Integer speed){
+    public Truck(String name, Integer speed){
         this.name = name;
         this.speed = speed;
     }
